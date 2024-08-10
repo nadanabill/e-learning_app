6e96b9430f6f85279123ce37046cbe1537a78f5e
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-08-10 07:40:16 +0300

    added app assets file

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,15 @@
+class AssetsPaths {
+  static const String assets = 'assets';
+  static const String svgs = '$assets/svgs';
+  static const String images = '$assets/images';
+  static const String icons = '$assets/icons';
+  static const String lotties = '$assets/lotties';
+}
+
+class AppImages {}
+
+class AppIcons {}
+
+class AppLotties {}
+
+class AppSvgs {}
