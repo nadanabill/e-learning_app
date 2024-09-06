0e7b42393d6a6ce2b5d8b6623b7fecc6e4284113
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-09-06 18:34:56 +0300

    added home strings

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -2,4 +2,11 @@ class AppStrings {
   static const String appName = 'BahasaKu';
   static const String register = 'Register';
   static const String login = 'Login';
+  static const String learn = 'Learn';
+  static const String search = 'Search';
+  static const String profile = 'profile';
+  static const String achievement = 'Achievement';
+  static const String whatLocalLanguage =
+      'What local language would you like to learn?';
+  static const String languageBeingLearned = 'Language Being Learned';
 }
