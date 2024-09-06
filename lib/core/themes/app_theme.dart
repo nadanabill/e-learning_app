b2c6c57d1be9bd76decdc0310e9390b28c052e53
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-09-06 18:33:58 +0300

    edited theme

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -1,6 +1,29 @@
 import 'package:e_learning/core/themes/app_colors.dart';
+import 'package:e_learning/core/themes/font_weight_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
- ThemeData appTheme() => ThemeData(
-   primaryColor: AppColors.primary100,
- );
+ThemeData appTheme() => ThemeData(
+      primaryColor: AppColors.primary100,
+      appBarTheme: const AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarIconBrightness: Brightness.dark,
+          statusBarBrightness: Brightness.dark,
+          statusBarColor: Colors.transparent,
+        ),
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(
+          color: AppColors.primary60,
+          fontSize: 12.sp,
+          fontWeight: FontWeightHelper.regular,
+        ),
+        unselectedLabelStyle: TextStyle(
+          color: AppColors.black100,
+          fontSize: 12.sp,
+          fontWeight: FontWeightHelper.regular,
+        ),
+      ),
+    );
