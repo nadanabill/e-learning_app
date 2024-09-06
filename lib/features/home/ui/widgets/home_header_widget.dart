3b5c6c78ad663992c4c83ab003559d20a5f49a6e
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-09-06 18:36:25 +0300

    added home header widget

---
+++ lib/features/home/ui/widgets/home_header_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/constants/app_assets.dart';
+import '../../../../core/constants/app_strings.dart';
+import '../../../../core/themes/app_text_styles.dart';
+
+class HomeHeaderWidget extends StatelessWidget {
+  final String name;
+  const HomeHeaderWidget({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return      Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(left: 30.w, bottom: 10.h),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Hi, $name!",
+                  style: AppTextStyles.font22Medium,
+                ),
+                Text(
+                  AppStrings.whatLocalLanguage,
+                  style: AppTextStyles.font14Regular,
+                ),
+              ],
+            ),
+          ),
+          Image.asset(AppImages.home),
+        ],
+      ),
+    );
+  }
+}
