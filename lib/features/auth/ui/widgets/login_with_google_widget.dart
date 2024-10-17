f2e97076c04498adb4c5fd0516020bb2051345e3
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-10-18 01:35:45 +0300

    added login with google widget

---
+++ lib/features/auth/ui/widgets/login_with_google_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/constants/app_assets.dart';
+import '../../../../core/constants/app_strings.dart';
+import '../../../../core/helpers/spaces.dart';
+import '../../../../core/themes/app_text_styles.dart';
+
+class LoginWithGoogleWidget extends StatelessWidget {
+  const LoginWithGoogleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            AppStrings.loginWithGoogle,
+            style: AppTextStyles.font14Medium,
+          ),
+          horizontalSpace(5),
+          Image.asset(AppImages.googleLogo),
+        ],
+      ),
+    );
+  }
+}
