7e3996445873c2b910dc89ecb9d399035986530c
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-10-18 01:33:04 +0300

    added default text form field widget

---
+++ lib/core/widgets/default_text_form_field_widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import '../themes/app_colors.dart';
+import '../themes/app_text_styles.dart';
+
+class DefaultTextFormFieldWidget extends StatefulWidget {
+  final TextInputType textInputType;
+  final String? Function(String?)? validator;
+  final TextEditingController controller;
+  final void Function(String)? onFieldSubmitted;
+  bool? obscureText;
+
+  DefaultTextFormFieldWidget({
+    super.key,
+    required this.controller,
+    this.textInputType = TextInputType.text,
+    this.validator,
+    this.obscureText,
+    this.onFieldSubmitted,
+  });
+
+  @override
+  State<DefaultTextFormFieldWidget> createState() =>
+      _DefaultTextFormFieldWidgetState();
+}
+
+class _DefaultTextFormFieldWidgetState
+    extends State<DefaultTextFormFieldWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: widget.textInputType,
+      style: AppTextStyles.font16Regular.copyWith(
+          color: AppColors.black100, decorationColor: AppColors.primary100),
+      controller: widget.controller,
+      validator: widget.validator,
+      obscureText: widget.obscureText ?? false,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: InputDecoration(
+        suffixIcon: widget.obscureText != null
+            ? IconButton(
+                icon: Icon(
+                  widget.obscureText!
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_rounded,
+                  color: AppColors.black80,
+                ),
+                onPressed: () {
+                  widget.obscureText = !widget.obscureText!;
+                  setState(() {});
+                },
+              )
+            : null,
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primary100),
+          borderRadius: BorderRadius.circular(40),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+    );
+  }
+}
