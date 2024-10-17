423244d4f38c0e7e791b830714ee4984e1026ffe
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-10-18 01:28:09 +0300

    added new text Styles and remove font family comment

--- lib/core/themes/app_text_styles.dart
+++ lib/core/themes/app_text_styles.dart
@@ -11,78 +11,90 @@ class AppTextStyles {
     fontSize: 24.sp,
     fontWeight: FontWeightHelper.bold,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font22Medium = TextStyle(
     fontSize: 22.sp,
     fontWeight: FontWeightHelper.medium,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font10Medium = TextStyle(
     fontSize: 10.sp,
     fontWeight: FontWeightHelper.medium,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font12Medium = TextStyle(
     fontSize: 12.sp,
     fontWeight: FontWeightHelper.medium,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
+  );
+  static TextStyle font14Medium = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: AppColors.black100,
+    fontFamily: fontFamily,
+  );
+  static TextStyle font16Medium = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.medium,
+    color: AppColors.primary100,
+    fontFamily: fontFamily,
   );
   static TextStyle font17Medium = TextStyle(
     fontSize: 17.sp,
     fontWeight: FontWeightHelper.medium,
     color: AppColors.black100,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font20Medium = TextStyle(
     fontSize: 20.sp,
     fontWeight: FontWeightHelper.medium,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font10Regular = TextStyle(
     fontSize: 10.sp,
     fontWeight: FontWeightHelper.regular,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font13Regular = TextStyle(
     fontSize: 13.sp,
     fontWeight: FontWeightHelper.regular,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font14Regular = TextStyle(
     fontSize: 14.sp,
     fontWeight: FontWeightHelper.regular,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font16Regular = TextStyle(
     fontSize: 16.sp,
     fontWeight: FontWeightHelper.regular,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font18Regular = TextStyle(
     fontSize: 18.sp,
     fontWeight: FontWeightHelper.regular,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font20Regular = TextStyle(
     fontSize: 20.sp,
     fontWeight: FontWeightHelper.regular,
     color: AppColors.white,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
   static TextStyle font28Regular = TextStyle(
     fontSize: 28.sp,
     fontWeight: FontWeightHelper.regular,
     color: AppColors.black10,
-    //fontFamily: fontFamily,
+    fontFamily: fontFamily,
   );
 }
