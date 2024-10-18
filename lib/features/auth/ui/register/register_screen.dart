d4a4c9ea3c366cca62838d65f71a94e637dedba0
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-10-18 09:52:19 +0300

    added register screen

---
+++ lib/features/auth/ui/register/register_screen.dart
@@ -0,0 +1,22 @@
+import 'package:e_learning/features/auth/ui/register/Widgets/register_body_widget.dart';
+import 'package:e_learning/features/auth/ui/register/Widgets/register_header_widget.dart';
+import 'package:flutter/material.dart';
+
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RegisterHeaderWidget(),
+            RegisterBodyWidget(),
+          ],
+        ),
+
+    );
+  }
+}
