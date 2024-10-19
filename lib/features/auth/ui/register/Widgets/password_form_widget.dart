e28cf711dc0ddbc5e3200833f8fdc481820845e3
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-10-19 03:47:05 +0300

    added password form widget

---
+++ lib/features/auth/ui/register/Widgets/password_form_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../../../../../core/constants/app_strings.dart';
+import '../../../../../core/helpers/spaces.dart';
+import '../../../../../core/helpers/validations.dart';
+import '../../../../../core/themes/app_text_styles.dart';
+import '../../../../../core/widgets/app_bar_icon_widget.dart';
+import '../../../../../core/widgets/default_button_widget.dart';
+import '../../../../../core/widgets/default_text_form_field_widget.dart';
+
+class PasswordFormWidget extends StatelessWidget {
+  const PasswordFormWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final passwordController = TextEditingController();
+    final passwordFormKey = GlobalKey<FormState>();
+    return Form(
+      key: passwordFormKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: AppBarIconWidget(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              icon: Icons.arrow_back_ios_rounded,
+            ),
+          ),
+          verticalSpace(30),
+          Text(
+            AppStrings.setUpYourPassword,
+            style: AppTextStyles.font22Medium,
+          ),
+          verticalSpace(30),
+          DefaultTextFormFieldWidget(
+            controller: passwordController,
+            obscureText: true,
+            validator: (value) => Validations.validatePassword(value),
+          ),
+          verticalSpace(30),
+          DefaultButtonWidget(
+            text: AppStrings.next,
+            onPressed: () {
+              if (passwordFormKey.currentState!.validate()) {}
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
