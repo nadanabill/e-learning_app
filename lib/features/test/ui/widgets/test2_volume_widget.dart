547a2d998ca28ace027bf7647fdde34d0f2e062b
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-10-29 22:23:29 +0300

    added test audio

--- lib/features/test/ui/widgets/test2_volume_widget.dart
+++ lib/features/test/ui/widgets/test2_volume_widget.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/constants/app_assets.dart';
+import '../../../../core/helpers/audio_manager.dart';
 import '../../../../core/helpers/spaces.dart';
 import '../../../../core/themes/app_colors.dart';
 import '../../../../core/themes/app_text_styles.dart';
 
-class Test2VolumeWidget extends StatelessWidget {
+class Test2VolumeWidget extends StatefulWidget {
   final String subQuestion;
 
   const Test2VolumeWidget({super.key, required this.subQuestion});
 
   @override
+  State<Test2VolumeWidget> createState() => _Test2VolumeWidgetState();
+}
+
+class _Test2VolumeWidgetState extends State<Test2VolumeWidget> {
+  final AudioManager _audioManager = AudioManager();
+
+  @override
+  void dispose() {
+    _audioManager.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Image.asset(AppImages.volumeButton),
+        InkWell(
+            onTap: () {
+              _audioManager.playLocalAudio('audio/test.mp3');
+            },
+            child: Image.asset(AppImages.volumeButton)),
         horizontalSpace(10),
         Expanded(
           child: Text(
-            subQuestion,
+            widget.subQuestion,
             style:
                 AppTextStyles.font18Regular.copyWith(color: AppColors.black100),
           ),
