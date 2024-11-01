cba1056072304749182bae48dd5b058db8d70f1b
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-11-01 12:46:05 +0200

    added test 3 model

---
+++ lib/features/test/models/test3_model.dart
@@ -0,0 +1,13 @@
+class Test3Model {
+  final String question;
+  final String subQuestion;
+  final String answer;
+  final List<Map<String, dynamic>> items;
+
+  Test3Model({
+    required this.question,
+    required this.subQuestion,
+    required this.answer,
+    required this.items,
+  });
+}
