2d70e51ad2dbe26704cf5bdfa684074b06e32ff0
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-11-17 06:18:51 +0200

    added search images

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -29,6 +29,11 @@ class AppImages {
   static const String xCircle = '${AssetsPaths.images}/' 'x_circle.png';
   static const String checkCircle = '${AssetsPaths.images}/' 'check_circle.png';
   static const String person = '${AssetsPaths.images}/person.png';
+  static const String searchBackground = '${AssetsPaths.images}/search_background.png';
+  static const String language1 = '${AssetsPaths.images}/language1.png';
+  static const String language2 = '${AssetsPaths.images}/language2.png';
+  static const String language3 = '${AssetsPaths.images}/language3.png';
+
 }
 
 class AppIcons {}
