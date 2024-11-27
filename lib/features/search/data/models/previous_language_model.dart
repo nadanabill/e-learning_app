00dff91c9c638e0bcc20d49d5828525f12ddedd2
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-11-28 00:45:56 +0200

    added previous language model and their data

---
+++ lib/features/search/data/models/previous_language_model.dart
@@ -0,0 +1,29 @@
+class PreviousLanguageModel {
+  final String name;
+  final String subTitle;
+  final String image;
+
+  PreviousLanguageModel({
+    required this.name,
+    required this.subTitle,
+    required this.image,
+  });
+}
+
+List<PreviousLanguageModel> previousLanguageCourses =[
+  PreviousLanguageModel(
+    name: 'Sundanese',
+    image: 'assets/images/sundanese.png',
+    subTitle: 'Introduction',
+  ),
+  PreviousLanguageModel(
+    name: 'Balinese',
+    image: 'assets/images/balinese.png',
+    subTitle: 'Introduction',
+  ),
+  PreviousLanguageModel(
+    name: 'Aceh',
+    image: 'assets/images/aceh.png',
+    subTitle: 'Introduction',
+  ),
+];
