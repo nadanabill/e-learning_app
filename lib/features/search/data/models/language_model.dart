36ccb41fd480df8d54b805a417d4953c30f2c7d6
Nada Nabil <nadanabilhelmyy@gmail.com>
2024-11-28 00:46:06 +0200

    added language model and their data

---
+++ lib/features/search/data/models/language_model.dart
@@ -0,0 +1,18 @@
+import 'package:e_learning/core/constants/app_assets.dart';
+
+class LanguageModel {
+  final String name;
+  final String image;
+
+  LanguageModel({
+    required this.name,
+    required this.image,
+  });
+}
+
+List<LanguageModel> languages=[
+  LanguageModel(name: 'Sundanese', image: AppImages.language1),
+  LanguageModel(name: 'Sundanese', image: AppImages.language2),
+  LanguageModel(name: 'Sundanese', image: AppImages.language3),
+];
+
